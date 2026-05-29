f79e71e3237beb9944ae55183bff5358d6957c8a
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2026-05-29 23:41:42 +0530

    Switch minification from Terser to esbuild

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -22,9 +22,9 @@ export default defineConfig({
     },
   },
   build: {
-    minify: "terser",
-    terserOptions: {
-      compress: { drop_console: true, drop_debugger: true },
+    minify: "esbuild",
+    esbuildOptions: {
+      drop: ["console", "debugger"],
     },
     rollupOptions: {
       output: {
